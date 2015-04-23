96c4322ff333cd43e716e47afeb7cf2e68aafea4
Tom <tj@tjshafer.com>
2015-04-23 07:35:12 -0700

    Update remote.php

--- config/remote.php
+++ config/remote.php
@@ -27,8 +27,9 @@
 				'username'  => '',
 				'password'  => '',
 				'key'       => '',
+				'keytext'   => '',
 				'keyphrase' => '',
-				'root'      => '/home/forge',
+				'agent'     => '',
 			],
 		],
 		/*
